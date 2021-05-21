e5434b65c11d94d96f9b0502f600c6168cd2f588
born_ <born_pc_2>
2021-05-21 16:43:25 -0500

    //Se ha cambiado de windows form a console

--- calculosueldo/Form1.cs
+++ calculosueldo/Form1.cs
@@ -15,6 +15,13 @@ namespace calculosueldo
         public Form1()
         {
             InitializeComponent();
+            double horas, pago, sueldo; //variables locales para calcular pago por hora
+            Console.WriteLine("Ingrese valor de horas trabajadas");//se muestra mensaje donde se pide las horas trabajadas
+            horas = Double.Parse(Console.ReadLine());//se guarda en variable el valor de las horas trabajadas
+            Console.WriteLine("Ingrese precio por cada hora");//se muestra msg que pide el precio por cada hora
+            pago = Double.Parse(Console.ReadLine());//se guarda en variable el precio por hora
+            sueldo = pago * horas;//se calcula el sueldo total
+            Console.WriteLine("El sueldo es de: " +sueldo );
         }
     }
 }
