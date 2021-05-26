0c9476689b36b9eb9c0e22fecae142fe613dfb52
born_ <born_pc_2>
2021-05-26 12:50:44 -0500

    //solucion de consola

---
+++ aplicacion_consola/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    
+    class Program
+    {
+        public static String nombre;
+
+        class registro
+        {
+            public void PedirNombre()
+            {
+                Console.WriteLine("Ingrese su primer nombre");
+               nombre= Console.ReadLine();
+            }
+        }
+        class mostrar
+        {
+            public void MostarNombre()
+            {
+                Console.WriteLine(nombre);
+            }
+        }
+
+  
+        static void Main(string[] args)
+        {
+            registro reg = new registro();
+            mostrar mos = new mostrar();
+            if (nombre==null)
+            {
+                Console.WriteLine("No hay nombres en variable inicializada.");
+                reg.PedirNombre();
+                mos.MostarNombre();
+            }
+                       
+
+        }
+    }
+}
