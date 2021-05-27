69bf69e18ac2f057042c397604144ad4bd2c20e3
born_ <born_pc_2>
2021-05-27 18:02:15 -0500

    //conexion a abse de datos y muestreo de empresas (bhd)

---
+++ PANEL_ADO/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace PANEL_ADO
+{
+    class Class1 
+    {
+        //conexion
+        public bool validar(String user, String pass)
+        {
+            bool sw;
+            string cadenaconexion = @"Data Source=BRYANWM-PC\MSSQLSERVER_G;Initial Catalog= ;User=" + user + " ;Password=" + pass + ";Integrated Security=False";//cadena de conexion
+            String query = @"Select name from sys.databases;";
+            SqlConnection conect = new SqlConnection(cadenaconexion);
+            try
+            {   conect.Open();
+                SqlCommand cmd = new SqlCommand(query, conect);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while(dr.Read())
+                {
+                    Console.WriteLine(dr.GetString(0));
+
+                }
+
+               
+                sw= true;
+            }
+            catch (SqlException e)
+            {   
+                sw = false;
+              
+            }
+            return sw ;
+        }
+        
+
+    }
+}
