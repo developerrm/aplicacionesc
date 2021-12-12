b8838376b2e7ccff0b7dc1ec123b01fc764ed8e5
ROGER MAZA <born_to_kill1998@outlook.com>
2021-12-12 18:20:06 -0500

    //CREADOR DE NUEVO MENU

---
+++ PANEL_ADO/UtilitarioCortarCodigo.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace PANEL_ADO
+{
+    partial class UtilitarioCortarCodigo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EntradaText = new System.Windows.Forms.RichTextBox();
+            this.SalidaText = new System.Windows.Forms.RichTextBox();
+            this.bntGenerar = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // EntradaText
+            // 
+            this.EntradaText.Location = new System.Drawing.Point(12, 12);
+            this.EntradaText.Name = "EntradaText";
+            this.EntradaText.Size = new System.Drawing.Size(845, 186);
+            this.EntradaText.TabIndex = 0;
+            this.EntradaText.Text = "";
+            // 
+            // SalidaText
+            // 
+            this.SalidaText.Location = new System.Drawing.Point(3, 257);
+            this.SalidaText.Name = "SalidaText";
+            this.SalidaText.Size = new System.Drawing.Size(845, 272);
+            this.SalidaText.TabIndex = 1;
+            this.SalidaText.Text = "";
+            // 
+            // bntGenerar
+            // 
+            this.bntGenerar.Location = new System.Drawing.Point(299, 217);
+            this.bntGenerar.Name = "bntGenerar";
+            this.bntGenerar.Size = new System.Drawing.Size(193, 23);
+            this.bntGenerar.TabIndex = 2;
+            this.bntGenerar.Text = "Generar";
+            this.bntGenerar.Click += new System.EventHandler(this.bntGenerar_Click);
+            // 
+            // UtilitarioCortarCodigo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 541);
+            this.Controls.Add(this.bntGenerar);
+            this.Controls.Add(this.SalidaText);
+            this.Controls.Add(this.EntradaText);
+            this.Name = "UtilitarioCortarCodigo";
+            this.Text = "UtilitarioCortarCodigo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox EntradaText;
+        private System.Windows.Forms.RichTextBox SalidaText;
+        private DevExpress.XtraEditors.SimpleButton bntGenerar;
+    }
+}
---
+++ PANEL_ADO/UtilitarioCortarCodigo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PANEL_ADO
+{
+    public partial class UtilitarioCortarCodigo : Form
+    {
+        public UtilitarioCortarCodigo()
+        {
+            InitializeComponent();
+        }
+
+        private void bntGenerar_Click(object sender, EventArgs e)
+        {
+            var entrada = EntradaText.Text.Trim();
+            var salida = "";
+            var tmp  = "";
+            char[] b = new char[entrada.Length];
+            SalidaText.Clear();
+            string path = @"C:\Users\ROGER-PC1\Downloads\MENU.txt";
+            if (File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    using (StringReader sr = new StringReader(entrada.ToString()))
+                    {
+                        // Read 13 characters from the string into the array.
+                        sr.Read(b, 0, entrada.Length);
+
+                        for (int i = 0; i < b.Length; i++)
+                        {
+                            tmp = tmp + b[i].ToString();
+                            if (tmp.ToUpper().Contains("CASE"))
+                            {
+                                salida = salida + "case" + "\t";
+                                tmp = "";
+                            }
+                            if (("1,2,3,4,5,6,7,8,9,0").Contains(b[i]))
+                            {
+                                salida = salida + b[i].ToString();
+                                tmp = "";
+                            }
+                            if (tmp.Contains("new"))
+                            {
+                                tmp = "";
+                                for (int j = i + 1; j < b.Length; j++)
+                                {
+                                    tmp = tmp + b[j].ToString();
+                                    if (tmp.Contains("."))
+                                    {
+                                        salida = salida + "\t" + tmp.ToString();
+                                        i = j;
+                                        break;
+                                    }
+                                }
+
+                                for (int k = i; k < b.Length; k++)
+                                {
+                                    tmp = tmp + b[k].ToString();
+                                    if (tmp.ToUpper().Contains("BREAK;"))
+                                    {
+                                        salida = salida + "\n";
+                                        tmp = "";
+                                        i = k;
+                                        break;
+                                    }
+
+                                }
+                            }
+                        }
+
+                    }
+                    sw.WriteLine(salida);
+                    
+                }
+            }
+            
+        }
+    }
+}
